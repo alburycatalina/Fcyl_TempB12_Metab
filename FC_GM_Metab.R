5095c5d908f17aaf43962d993753f54839d93cfd
Catalina Albury <alburycatalina@gmail.com>
2021-10-19 16:39:15 -0300

    first steps

--- FC_GM_Metab.R
+++ FC_GM_Metab.R
@@ -1,5 +1,14 @@
 library(usethis)
+library(gh)
 
+
+# Se4t working directory 
 setwd("~/Desktop/OneDrive - Dalhousie University/MSc/1_CHII/FC_GM_Data/FC_GM_Qtrap/NRC_Data/Fcyl_TempB12_Metab")
 
-metab_data <- ("21080901_R1002_Rowland-Filt-Norm.csv")
+# Read in normalized metabolite data
+metab_data <- read.csv("21080901_R1002_Rowland-Filt-Norm.csv")
+
+# Check for NA's in data columns (there are none )
+any(is.na(metab_data[,4:88]))
+
+
