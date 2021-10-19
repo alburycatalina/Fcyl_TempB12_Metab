53c2018d324911d9645e5564bb7453ffaa635681
Catalina Albury <alburycatalina@gmail.com>
2021-10-19 11:41:35 -0300

    added usethis

--- FC_GM_Metab.R
+++ FC_GM_Metab.R
@@ -1,3 +1,5 @@
+library(usethis)
+
 setwd("~/Desktop/OneDrive - Dalhousie University/MSc/1_CHII/FC_GM_Data/FC_GM_Qtrap/NRC_Data/Fcyl_TempB12_Metab")
 
 metab_data <- ("21080901_R1002_Rowland-Filt-Norm.csv")
